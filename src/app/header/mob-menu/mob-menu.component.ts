5c1d7442b2de5ed1572139d99215f6f045c5ea6b
Nitesh Thakare <nthakare09@gmail.com>
2024-12-21 22:02:24 +0530

    bottom menu created

--- src/app/header/mob-menu/mob-menu.component.ts
+++ src/app/header/mob-menu/mob-menu.component.ts
@@ -1,57 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
-import { MenubarModule } from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-mob-menu',
   standalone: true,
-  imports: [MenubarModule],
+  imports: [MenuModule, ButtonModule],
   templateUrl: './mob-menu.component.html',
   styleUrl: './mob-menu.component.scss'
 })
 export class MobMenuComponent implements OnInit {
   items: MenuItem[] | undefined;
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-home'
-      },
-      {
-        label: 'Training',
-        icon: 'pi pi-graduation-cap'
-      },
-      {
-        label: 'Enquiry',
-        icon: 'pi pi-phone'
-      },
-      {
-        label: 'Assessment',
-        icon: 'pi pi-pen-to-square'
-      },
-      {
-        label: 'More',
-        icon: 'pi pi-bars',
-        items: [
-          {
-            label: 'Community',
-            icon: 'pi pi-bolt'
-          },
-          {
-            label: 'Apply for Job',
-            icon: 'pi pi-server'
-          },
-          {
-            label: 'Verify Certificate',
-            icon: 'pi pi-pencil'
-          },
-          {
-            label: 'Templates',
-            icon: 'Help Desk'
-          }
-        ]
-      }
-    ]
-  }
+    ngOnInit() {
+        this.items = [
+            {
+                items: [
+                    {
+                        label: 'Community'
+                    },
+                    {
+                        label: 'Apply for Job'
+                    },
+                    {
+                        label: 'Verify Certificate'
+                    },
+                    {
+                        label: 'Help Desk'
+                    }
+                ]
+            }
+        ];
+    }
 }
