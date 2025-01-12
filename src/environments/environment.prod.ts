829af5a3de59305fb3be2cdc9b02e01936353fba
Tarique Anwer <tarique.anwer@instantsys.com>
2025-01-12 19:23:33 +0530

    Fix blogs image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://api.technical99.com'
+    baseUrl: 'https://api.technical99.com',
+    blogImageUrl: 'https://technical99.com/images/blog/'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://api.technical99.com/'
+    baseUrl: 'https://api.technical99.com/',
+    blogImageUrl: 'https://technical99.com/images/blog/'
   };
